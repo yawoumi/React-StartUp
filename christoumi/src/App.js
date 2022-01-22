a20cc1756388d01bf7d5cc95749251fc202f1a1d
Christelle Randriamahefa <christelle.randriamahefa@gmail.com>
2022-01-22 16:44:53 +0100

    Team + Contact update

--- christoumi/src/App.js
+++ christoumi/src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Contact from './pages/Contact' ;
+import Team from './pages/Team';
 import Home from './pages/Home';
 import Footer from './components/Footer';
 
 function App() {
   return (
     <div>
-      <Home />
+      <Team/>
       <Footer />
     </div>
   )
--- christoumi/src/components/Footer.js
+++ christoumi/src/components/Footer.js
@@ -1,13 +1,9 @@
 import React from 'react';
-import styled from "styled-components";
 import Container from '@material-ui/core/Container';
 import Grid from '@material-ui/core/Grid';
 import Box from '@material-ui/core/Box';
 import Link from '@material-ui/core/Link';
-import PropTypes from 'prop-types';
-import { withStyles } from '@material-ui/core/styles';
-import Paper from '@material-ui/core/Paper';
-import Typography from '@material-ui/core/Typography';
+import logo from './img/logoChristoumiv2-removebg-preview.png'
  
 export default function Footer() {
   return (
@@ -15,11 +11,25 @@ export default function Footer() {
       <Box
         px={{ xs: 3, sm: 10 }}
         py={{ xs: 5, sm: 10 }}
-        bgcolor="text.secondary"
-        color="white"
+        bgcolor="#D3D1D1"
+        color="#434343"
       >
         <Container maxWidth="lg">
           <Grid container spacing={5}>
+            <Grid item xs={12} sm={4}>
+              {/*<Box borderBottom={1}>Account</Box>*/}
+              
+              <Box>
+                <img src={logo} width="200" height="100"/>
+              </Box>
+              <Box>
+                <p>
+                  Christoumi <br></br>
+                  5 chemin de Bellevue, Annecy-le-Vieux <br></br>
+                  74940 ANNECY
+                </p>
+              </Box>
+            </Grid>
             <Grid item xs={12} sm={4}>
               <Box borderBottom={1}>About Us</Box>
               <Box>
@@ -28,7 +38,7 @@ export default function Footer() {
                 </Link>
               </Box>
               <Box>
-                <Link href="/" color="inherit">
+                <Link href="/pages/Contact.js" color="inherit">
                   Contact us
                 </Link>
               </Box>
@@ -38,19 +48,7 @@ export default function Footer() {
                 </Link>
               </Box>
             </Grid>
-            <Grid item xs={12} sm={4}>
-              <Box borderBottom={1}>Account</Box>
-              <Box>
-                <Link href="/" color="inherit">
-                  Login
-                </Link>
-              </Box>
-              <Box>
-                <Link href="/" color="inherit">
-                  Register
-                </Link>
-              </Box>
-            </Grid>
+           
             <Grid item xs={12} sm={4}>
               <Box borderBottom={1}>Social Media</Box>
               <Box>
